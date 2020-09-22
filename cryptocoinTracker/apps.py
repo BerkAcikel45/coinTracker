c127ed6c92c787fa2cda6dc77bad546a886fc17f
BerkAcikel45 <22922136+BerkAcikel45@users.noreply.github.com>
2020-09-22 23:54:11 +0300

    initial

---
+++ cryptocoinTracker/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CryptocointrackerConfig(AppConfig):
+    name = 'cryptocoinTracker'
---
+++ cryptocoinTracker/tasks.py
@@ -0,0 +1,14 @@
+from celery import shared_task
+from cryptocompy import price
+from django.core.cache import cache
+
+
+@shared_task
+def update_cc_prices():
+    cryptocoins = ['ETH', 'BTC']
+    currencies = ['EUR', 'USD']
+    response = price.get_current_price(cryptocoins, currencies)
+    for cryptocoin in cryptocoins:
+        for currency in currencies:
+            ticker_code = cryptocoin + currency
+            cache.set(ticker_code, response[cryptocoin][currency])
